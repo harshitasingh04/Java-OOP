95f7d6cdda2303c1ba1759d5c248f948eb0a7833
harshitasingh04 <114250253+harshitasingh04@users.noreply.github.com>
2023-08-11 13:07:39 +0530

    Add files via upload

---
+++ WrapperExamples.java
@@ -0,0 +1,10 @@
+package introduction;
+public class WrapperExamples {
+    public static void main(String[] args) {
+         int a = 10;
+         Integer num = 45;
+         num.byteValue();
+         final int bonus = 2; 
+        // always initialize while declaring it
+    }
+}
